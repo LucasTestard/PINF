3dee41177defeca188bd8cb49f00d19199e5e6ca
ybendris <57152433+ybendris@users.noreply.github.com>
2020-04-19 14:00:35 +0200

    Add files via upload

--- Travelis/travelis.sql
+++ Travelis/travelis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 24 fév. 2020 à 17:37
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.3.12
+-- Généré le :  Dim 19 avr. 2020 à 11:59
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `login` varchar(255) NOT NULL,
   `passe` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
@@ -130,7 +130,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `nom`, `prenom`, `droit`, `login`, `passe`) VALUES
 (1, 'Bendris', 'Yannis', 2, 'Yannis', 'mysql'),
 (2, 'aa', 'aa', 0, 'aa', 'aa'),
-(3, 'aa', 'aa', 0, 'aa', 'aa');
+(3, 'aa', 'aa', 0, 'aa', 'aa'),
+(4, 'Ben', 'Yan', 0, 'yben', 'mysql');
 
 -- --------------------------------------------------------
 
